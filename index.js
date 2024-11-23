137564843719fe165987d58e06b4a64160d1a088
Rohith <tangadipallirohith@gmail.com>
2024-11-23 17:40:03 -0500

    public1

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ const server = http.createServer((req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 5959;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
